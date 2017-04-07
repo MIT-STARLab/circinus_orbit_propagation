c3214493726c06b054a003f5bd09baa214c46e9c
Kit Kennedy <akennedy@mit.edu>
2017-04-07 16:26:00 -0400

    adding script for utc to mjd conversion

---
+++ matlab_tools/utc_str_to_mjd.m
@@ -0,0 +1,9 @@
+time = '15 Mar 2017 11:21:15.000'
+
+% addpath('../Libraries/PROPAT/propat_code');
+% dt = datetime(time,'InputFormat','dd MMM yyyy HH:mm:ss.sss');
+% mjdi = djm(day(dt), month(dt), year(dt))  % get modified julian day for ECEF -> ECI conversion
+
+% juliandate(time)
+
+mjuliandate(time)
