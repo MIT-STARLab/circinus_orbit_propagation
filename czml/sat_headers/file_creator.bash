aa3fe84c94b8906037d0ae882fc78a694076e2d4
Kit Kennedy <akennedy@mit.edu>
2017-06-11 19:39:03 -0400

    sat headers files up to 100

---
+++ czml/sat_headers/file_creator.bash
@@ -0,0 +1,79 @@
+#!/bin/bash
+cp ./sat45_pos_stub.czml.part.txt ./sat62_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat62/g" sat62_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat63_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat63/g" sat63_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat64_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat64/g" sat64_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat65_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat65/g" sat65_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat66_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat66/g" sat66_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat67_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat67/g" sat67_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat68_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat68/g" sat68_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat69_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat69/g" sat69_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat70_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat70/g" sat70_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat71_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat71/g" sat71_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat72_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat72/g" sat72_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat73_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat73/g" sat73_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat74_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat74/g" sat74_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat75_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat75/g" sat75_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat76_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat76/g" sat76_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat77_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat77/g" sat77_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat78_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat78/g" sat78_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat79_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat79/g" sat79_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat80_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat80/g" sat80_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat81_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat81/g" sat81_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat82_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat82/g" sat82_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat83_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat83/g" sat83_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat84_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat84/g" sat84_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat85_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat85/g" sat85_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat86_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat86/g" sat86_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat87_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat87/g" sat87_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat88_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat88/g" sat88_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat89_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat89/g" sat89_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat90_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat90/g" sat90_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat91_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat91/g" sat91_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat92_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat92/g" sat92_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat93_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat93/g" sat93_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat94_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat94/g" sat94_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat95_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat95/g" sat95_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat96_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat96/g" sat96_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat97_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat97/g" sat97_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat98_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat98/g" sat98_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat99_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat99/g" sat99_pos_stub.czml.part.txt
+cp ./sat45_pos_stub.czml.part.txt ./sat100_pos_stub.czml.part.txt
+sed -i -- "s/CubeSat45/CubeSat100/g" sat100_pos_stub.czml.part.txt
