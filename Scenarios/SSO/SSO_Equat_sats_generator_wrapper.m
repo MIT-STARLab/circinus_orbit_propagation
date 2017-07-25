773432a11d8e4afdb46df4b5d6cd720d28960745
Kit Kennedy <akennedy@mit.edu>
2017-07-25 17:40:41 -0400

    adding a bunch of stuff for new equatorial gs and targets

--- Scenarios/SSO/SSO_Equat_sats_generator_wrapper.m
+++ Scenarios/SSO/SSO_Equat_sats_generator_wrapper.m
@@ -12,8 +12,8 @@ sat_headers_loc = '/czml/sat_headers/';
 
 out_file_string1 = 'orbitprop_ssoequat';
 
-targets_parameters_filename = '../parameters_targets.xlsx';
-gs_parameters_filename = '../parameters_gs_network.xlsx'; % Change to 'parameters_descope.xlsx'
+targets_parameters_filename = '../parameters_targets_2.xlsx';
+gs_parameters_filename = '../parameters_gs_network_2.xlsx'; % Change to 'parameters_descope.xlsx'
 
 
 %% file inputs
@@ -24,7 +24,8 @@ gs_parameters_filename = '../parameters_gs_network.xlsx'; % Change to 'parameter
 %  header_file = '../../czml/czml_header_33targ_ksat.czml.part.txt';
 %  header_file = '../../czml/czml_header_33targ_equatalt.czml.part.txt';
 % header_file = '../../czml/czml_header_33targ_bridgesat.czml.part.txt';
-header_file = '../../czml/czml_header_33targ_wallops.czml.part.txt';
+% header_file = '../../czml/czml_header_33targ_wallops.czml.part.txt';
+header_file = '../../czml/czml_header_equat1targ_singapore.czml.part.txt';
 
 % gs=1 SFN
 % gs=2 KSAT
@@ -32,20 +33,23 @@ header_file = '../../czml/czml_header_33targ_wallops.czml.part.txt';
 % gs=5 equatorial alternative
 % gs=6 bridgesat
 % gs=8 wallops
+% gs=9 (equatorial 2 ... gs xlsx 2)
+% gs=10 (singapore ... gs xlsx 2)
 
-gs_network = 8;
+gs_network = 10;
 
 % orbit inputs
-num_sats_orbit_1 = 20;  % SSO @ 10:30
+num_sats_orbit_1 = 0;  % SSO @ 10:30
 num_sats_orbit_2 = 0;   % SSO @ 14:30
 num_sats_orbit_3 = 10;  % equatorial
 
 start_time_str = '15 Mar 2017 10:00:00.000';  % make sure to include the milliseconds! That's necessary to make python epoch updater script work
 
 delta_t_sec = 10;  % seconds
-end_time_sec = 86400*3;  % seconds
+end_time_sec = 86400*1;  % seconds
 
-targets_parameters_sheetname = '33_targ';
+% targets_parameters_sheetname = '33_targ';  %(target xlsx 1)
+targets_parameters_sheetname = 'equat_targets_1'; %(target xlsx 2)
 yes_crosslinks = 1;
 
 
@@ -66,7 +70,7 @@ xlnks_file_name = [out_file_string1,out_file_string2,out_file_string4,'_xlnk.mat
 num_sats = num_sats_orbit_1 + num_sats_orbit_2 + num_sats_orbit_3;
 
 gs_parameters_sheetname = num2str(gs_network);
-info_string = ['created for ',num2str(num_sats),' satellite scenario from matlab script SSO_Equat_sats_generator_wrapper.m, using targets params file sheet ',targets_parameters_sheetname,' and gs sheet ',gs_parameters_sheetname,', for ',num2str(end_time_sec),' seconds with ',num2str(delta_t_sec),' seconds timestep'];
+info_string = ['created for ',num2str(num_sats),' satellite scenario from matlab script SSO_Equat_sats_generator_wrapper.m, using targets params file sheet ',targets_parameters_filename,'/',targets_parameters_sheetname,' and gs sheet ',gs_parameters_filename,'/',gs_parameters_sheetname,', for ',num2str(end_time_sec),' seconds with ',num2str(delta_t_sec),' seconds timestep'];
 info_string = [info_string, '; orbits: '];
 
 SSO_Equat_file_writer
